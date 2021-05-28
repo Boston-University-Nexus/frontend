02f67dfa04452694f11e4a3614726d5dcaeef68b
Phillip Tran <ptrandev@protonmail.com>
2021-05-28 17:35:58 -0400

    font weight of search splash screens

--- src/pages/Planner/ClassSection/NotFound.js
+++ src/pages/Planner/ClassSection/NotFound.js
@@ -6,7 +6,7 @@ export default function NotFound() {
     <div className="flex items-end uppercase h-full text-center p-4">
       <div className="h-2/3 xl:1/2 2xl:h-1/3 w-full flex flex-col justify-between">
         <div>
-          <h2 className="font-black text-sm xl:text-lg">no results found!</h2>
+          <h2 className="font-bold text-sm xl:text-lg">no results found!</h2>
           <p className="text-xs xl:text-sm">
             nothing matched your search terms
           </p>
--- src/pages/Planner/ClassSection/StartTyping.js
+++ src/pages/Planner/ClassSection/StartTyping.js
@@ -6,7 +6,7 @@ export default function StartTyping() {
     <div className="flex items-end uppercase h-full text-center p-4">
       <div className="h-2/3 xl:1/2 2xl:h-1/3 w-full flex flex-col justify-between">
         <div>
-          <h2 className="font-black text-sm xl:text-lg">start searching...</h2>
+          <h2 className="font-bold text-sm xl:text-lg">start searching...</h2>
           <p className="text-xs xl:text-sm">
             find courses, departments & instructors
           </p>
