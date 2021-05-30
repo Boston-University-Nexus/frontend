3ca46a6f557a8055e91f68f4a997c0b3af73f928
Phillip Tran <ptrandev@protonmail.com>
2021-05-30 19:35:00 -0400

    recommended open by default

--- src/pages/Planner/Main.js
+++ src/pages/Planner/Main.js
@@ -30,7 +30,7 @@ class Main extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      recommendedOpen: false,
+      recommendedOpen: true,
     };
 
     this.toggleMenu = this.toggleMenu.bind(this);
