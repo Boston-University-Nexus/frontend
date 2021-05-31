06ead7f4f93bcdd400cbe636ce72f57b43be2340
Phillip Tran <ptrandev@protonmail.com>
2021-05-30 20:40:49 -0400

    schedule dropdown animation, click hitbox, chevron

--- src/pages/Planner/Calendar/ScheduleList.js
+++ src/pages/Planner/Calendar/ScheduleList.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 
 // Icons
-import { FaTrashAlt, FaCopy, FaChevronDown, FaEdit } from "react-icons/fa";
+import { FaTrashAlt, FaCopy, FaChevronDown, FaChevronUp, FaEdit } from "react-icons/fa";
 import { FiStar } from "react-icons/fi";
 
 // Functions
@@ -63,50 +63,52 @@ class ScheduleList extends Component {
   render() {
     return (
       <div
-        className="ml-5 relative bg-gray-200 text-gray-600 rounded-full px-4 py-0.5 flex items-center capitalize cursor-pointer select-none font-bold"
+        className="ml-5 relative bg-gray-200 text-gray-600 rounded-full flex items-center capitalize cursor-pointer select-none font-bold"
         ref={this.scheduleDrop}
       >
         {/* Dropdown */}
         <div
-          className="flex items-center w-full"
+          className="flex px-4 py-0.5 items-center w-full"
           onClick={() => this.setState({ open: !this.state.open })}
         >
           {this.props.activeCalendar.title}
-          <FaChevronDown className="ml-2"/>
+          {this.state.open
+            ? <FaChevronUp className="ml-2"/>
+            : <FaChevronDown className="ml-2"/>
+          }
         </div>
-        {this.state.open && (
-          <div className="absolute top-full mt-3 left-0 shadow-2xl rounded-lg flex flex-col w-64 overflow-hidden z-30 border">
-            {/* For every calendar saved in state */}
-            {this.props.calendars.map((item, key) => {
-              let selected = item.title === this.props.activeCalendar.title;
-              return (
-                <div
-                  className={
-                    "px-3 py-4 hover:bg-blue-100 flex items-center justify-between " +
-                    (selected ? "bg-blue-100" : "bg-white")
-                  }
-                  key={key}
-                  onClick={() => {
-                    this.props.changeCalendar(item);
-                  }}
-                >
-                  <div className="flex items-center w-2/3">
-                    <FiStar />
-                    <span className="ml-1 overflow-hidden overflow-ellipsis whitespace-nowrap">
-                      {item.title}
-                    </span>
-                  </div>
-
-                  <div className="flex text-gray-500 text-sm">
-                    <FaEdit />
-                    <FaCopy className="mx-1" />
-                    <FaTrashAlt />
-                  </div>
+        <div className={"absolute top-full mt-3 left-0 shadow-2xl rounded-lg flex flex-col w-64 overflow-hidden z-30 border transition-opacity " +
+        (this.state.open ? "opacity-100" : "invisible opacity-0")}>
+          {/* For every calendar saved in state */}
+          {this.props.calendars.map((item, key) => {
+            let selected = item.title === this.props.activeCalendar.title;
+            return (
+              <div
+                className={
+                  "px-3 py-4 hover:bg-blue-100 flex items-center justify-between " +
+                  (selected ? "bg-blue-100" : "bg-white")
+                }
+                key={key}
+                onClick={() => {
+                  this.props.changeCalendar(item);
+                }}
+              >
+                <div className="flex items-center w-2/3">
+                  <FiStar />
+                  <span className="ml-1 overflow-hidden overflow-ellipsis whitespace-nowrap">
+                    {item.title}
+                  </span>
                 </div>
-              );
-            })}
-          </div>
-        )}
+
+                <div className="flex text-gray-500 text-sm">
+                  <FaEdit />
+                  <FaCopy className="mx-1" />
+                  <FaTrashAlt />
+                </div>
+              </div>
+            );
+          })}
+        </div>
       </div>
     );
   }
