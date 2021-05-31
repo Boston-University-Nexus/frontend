968cd95a6e1463d50d4b54ece12996498b5de87c
Phillip Tran <ptrandev@protonmail.com>
2021-05-31 01:37:15 -0400

    classes now have pointer upon hover

--- src/pages/Planner/ClassSection/ClassItem.js
+++ src/pages/Planner/ClassSection/ClassItem.js
@@ -5,7 +5,7 @@ const ClassItem = (props) => {
   let classCode =
     props.item.college + " " + props.item.department + " " + props.item.number;
   return (
-    <div className="bg-white py-3 pl-4 pr-2 flex hover:bg-blue-100 transition">
+    <div className="bg-white py-3 pl-4 pr-2 flex hover:bg-blue-100 cursor-pointer transition">
       <div className="flex flex-col w-3/5">
         <span className="text-lg mr-3 font-bold">{classCode}</span>
         <span className="text-sm overflow-ellipsis w-full whitespace-nowrap overflow-hidden">
