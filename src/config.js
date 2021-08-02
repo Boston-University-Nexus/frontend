da80791a53c40c9a8e93404372485c856f1bb486
Daniel Melchor <danielmelchor@localhost.localdomain>
2021-08-02 02:39:36 +0200

    production test ready frontend

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 export default {
-  server: "https://api.bunexus.app/",
+  server: "http://localhost:8000/",
   coursesearch_max_items: 10,
   sql_max_items: 50,
 };
 
-// https://bunexus-api.herokuapp.com/
+// https://api.bunexus.app/
 // http://localhost:8000/
